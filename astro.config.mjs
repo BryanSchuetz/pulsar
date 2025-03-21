db07474449e2a1b5ce659edcd2074872ac4b1ed1
Bryan Schuetz <bschuetz@gmail.com>
2025-03-21 12:31:46 -0400

    updating configs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,19 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import devtoolBreakpoints from "astro-devtool-breakpoints";
+import rehypeMermaid from 'rehype-mermaid';
 
 import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://distresssignal.org',
-  integrations: [tailwind(), devtoolBreakpoints(), mdx()]
-});
+  integrations: [tailwind(), devtoolBreakpoints(), mdx()],
+  markdown: {
+    syntaxHighlight: {
+      type: 'shiki',
+      excludeLangs: ['mermaid', 'math']
+    },
+    rehypePlugins: [rehypeMermaid]
+  }
+  });
