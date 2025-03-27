bac6408e1d6076117c4d956ea38511e93d70532c
Bryan Schuetz <bschuetz@gmail.com>
2025-03-27 12:15:36 -0400

    ssr is fucking shit up

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,7 @@ import mdx from "@astrojs/mdx";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://distresssignal.org',
+  output: 'static',
   integrations: [tailwind(), mdx()],
   markdown: {
     syntaxHighlight: {
